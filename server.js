77ebba4cf7c2ed9de5134c4b792fc1ca2689276f
unknown <fizz.zehra14@gmail.com>
2021-09-28 13:14:55 -0500

    make it better

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI || 'mongodb://localhost/budget-tracker', {
+mongoose.connect(MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
