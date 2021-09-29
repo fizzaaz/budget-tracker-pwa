a286e748ad88fb5fbe1cc4ae25a4d015d604e88e
unknown <fizz.zehra14@gmail.com>
2021-09-28 19:15:05 -0500

    make it better

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,7 +17,6 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME)
             .then(function(cache) {
-                console.log('Opened cache');
                 return cache.addAll(FILES_TO_CACHE);
             })
     );
